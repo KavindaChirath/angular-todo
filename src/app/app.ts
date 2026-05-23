50704e0d62be0a88434d6025bca65bd55389d74b
kavinda chirath <ict22028@std.uwu.ac.lk>
2026-05-23 23:04:14 +0530

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { Todolist } from './todolist/todolist';
+
+@Component({
+  selector: 'app-root',
+
+  imports: [Todolist],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('todo');
+}
