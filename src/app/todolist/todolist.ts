25bb131d7b561b8d7d560c4f8d9c18e43e8300ce
kavinda chirath <ict22028@std.uwu.ac.lk>
2026-05-24 01:02:30 +0530

    create a Array for a tasks funtion the delete button

--- src/app/todolist/todolist.ts
+++ src/app/todolist/todolist.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todolist',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './todolist.html',
   styleUrl: './todolist.css',
 })
-export class Todolist {}
+export class Todolist {
+  taskArray = [{ task: 'Brush teeth' , isCompleted: false}];
+  
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+  }
+
+  toggleTask(task: any) {
+    task.isCompleted = !task.isCompleted;
+  }
+
+  deleteTask(task: any) {
+    this.taskArray = this.taskArray.filter(t => t !== task);
+  }
+}
